88822b94f6ee014932e702db66cc56d1e6ace029
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-05 23:18:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length >= 1 
+    new_line = []
+    counter = 1 
+    array.each do |name|
+      new_line.push("#{counter}. #{name}")
+      counter +=1 
+    end 
+    puts "The line is currently: " + new_line.join(" ")
+  else 
+    puts "The line is currently empty."
+  end 
+end
+
+def take_a_number (array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end 
